f1e0b35dc20447d6ac3e24e661626288878310d9
shubhamtewari <shbmtewari@gmail.com>
2018-10-23 12:15:20 +0530

    wrote Piece.java interface

---
+++ chessgame/src/Piece.java
@@ -0,0 +1,20 @@
+package chessgame.src;
+
+/**
+ *this interface defines a general chess piece
+ *each and every game piece implements this interface
+ *@author shubhamtewari
+ */
+interface GamePiece {
+
+  /**
+   *gets the x and y positions of the piece
+   *@return a pair object with the x and y positions
+   */
+   public Pair getCoordinates();
+
+   /**
+    *
+    */
+    
+}
